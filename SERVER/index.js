45942845d3d46010313a0bfe4006c50508195def
payal2067 <payalsanodiya89@gmail.com>
2026-06-27 21:45:07 +0530

    feat: add whatwg-url library and MongoDB connection configuration
    - Introduced the whatwg-url library with core utilities for URL handling.
    - Added package.json for whatwg-url with necessary dependencies and scripts.
    - Created webidl2js-wrapper.js to export URL and URLSearchParams from the library.
    - Implemented dbConnection.config.js for MongoDB connection using mongoose.

--- SERVER/index.js
+++ SERVER/index.js
@@ -2,29 +2,18 @@
 dotenv.config();
 
 import express from "express";
-
-
+import connectDB from "./src/config/dbConnection.config.js";
 
 const app = express();
 
-// app.use(express.json());
 
-// app.use("/auth", AuthRouter);
-// app.use("/public", PublicRouter);
 
 //Default API
 app.get("/", (req, res) => {
   console.log("Default Get API Hit");
-  res.json({ message: "Welcome to my Cravins Project" });
+  res.json({ message: "Welcome to my Cravins Project"});
 });
 
-//Default Error Handler
-// app.use((err, req, res, next) => {
-//   const ErrMessage = err.message || "Internal Server Error";
-//   const ErrStausCode = err.statusCode || 500;
-
-//   res.status(ErrStausCode).json({ message: ErrMessage });
-// });
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
